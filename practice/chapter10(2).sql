2c10b31625a3220f0e3ff38e52bfe9560e3ccc49
imaaa34 <oct.imd34@gmail.com>
2021-10-28 09:00:45 +0900

    [Finish] chapter10(2)

---
+++ practice/chapter10(2).sql
@@ -0,0 +1,24 @@
+-- 10-2
+CREATE TABLE 学部 (
+  ID CHAR(1) PRIMARY KEY,
+  名前 VARCHAR(20) NOT NULL UNIQUE,
+  備考 VARCHAR(100) NOT NULL DEFAULT '特になし' 
+);
+
+-- 10-3
+CREATE TABLE 学生 (
+  学籍番号 CHAR(8) PRIMARY KEY,
+  名前 VARCHAR(30) NOT NULL,
+  生年月日 DATE NOT NULL,
+  血液型 CHAR(2) CHECK(血液型 IN ('A', 'B', 'O', 'AB') OR 血液型 IS NULL),
+  学部ID CHAR(1) REFERENCES 学部(ID)
+);
+
+-- 10-4
+BEGIN;
+UPDATE 学生
+   SET 学部ID = 'K'
+ WHERE 学部ID = 'R';
+DELETE FROM 学部
+ WHERE ID = 'R';
+COMMIT;
